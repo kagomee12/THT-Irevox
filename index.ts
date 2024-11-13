9fdff828f1ea0791142fca0bb3b4d9f7588bfa6e
kagomee12 <kalfitrah@gmail.com>
2024-11-13 15:35:06 +0700

    chore: move src/index.ts to index.ts

---
+++ index.ts
@@ -0,0 +1,21 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, async () => {
+    console.log(`App listening on port ${port}`)
+})
