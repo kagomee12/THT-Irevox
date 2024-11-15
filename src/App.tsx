3046073e07c445828eea318c60b0f225a70bdcb0
kagomee12 <kalfitrah@gmail.com>
2024-11-15 07:25:28 +0700

    chore: set up Tailwind CSS for styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import "./App.css";
 import { route } from "./routes/route";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { setAuthToken } from "./services/api";
 
 function App() {
-  return <RouterProvider router={createBrowserRouter(route)} />;
+  const queryClient = new QueryClient();
+  setAuthToken(localStorage.getItem("token")?.toString());
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={createBrowserRouter(route)} />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
